a7f724ad1db633a426a2fe14dfe7490d6a0b71c7
Francis Ayodeji <fsixpro@gmail.com>
2020-09-19 12:27:14 +0100

    fixing videoScreen

--- src/bottomnav/Training.js
+++ src/bottomnav/Training.js
@@ -31,49 +31,50 @@ const Videos = ({navigation, getTraining, training, getVideo, video}) => {
         onRequestClose={() => {
           setModal(false);
         }}>
-        <VideoPlayerScreen VideoURI={video.video_file} />
+        <VideoPlayerScreen VideoURI={video} />
       </Modal>
       <Header title={'Training'} />
       <FlatList
         keyExtractor={(item) => item.id.toString()}
         data={training.training}
         renderItem={({item}) => (
-          <View
-            style={{
-              flexDirection: 'row',
-              backgroundColor: AppColor.WHITE,
-              marginTop: 5,
-              borderBottomWidth: 10,
-              borderBottomColor: AppColor.WHITE,
-            }}>
-            <TouchableOpacity onPress={() => videoHandler(item)}>
+          <TouchableOpacity onPress={() => videoHandler(item)}>
+            <View
+              style={{
+                flexDirection: 'row',
+                backgroundColor: AppColor.WHITE,
+                marginTop: 5,
+                borderBottomWidth: 10,
+                borderBottomColor: AppColor.WHITE,
+              }}>
               <Image
                 style={{width: 100, height: 100}}
                 source={require('../../assets/videoarrow.png')}
               />
-            </TouchableOpacity>
-            <View style={{top: 10, marginHorizontal: 7}}>
-              <Text
-                ellipsizeMode="clip"
-                numberOfLines={2}
-                style={{
-                  fontSize: 11,
-                  fontWeight: 'bold',
-                  width: 230,
-                }}>
-                {item.topic}
-              </Text>
-              <Text style={{color: AppColor.SECONDARY_COLOR}}>
-                {item.facilitator}
-              </Text>
-              <Text style={{color: AppColor.SECONDARY_COLOR}}>
-                {`${item.duration}:00 | ${item.skill} `}
-                <Text style={{color: AppColor.PRIMARY_COLOR}}>
-                  {item.price}
+
+              <View style={{top: 10, marginHorizontal: 7}}>
+                <Text
+                  ellipsizeMode="clip"
+                  numberOfLines={2}
+                  style={{
+                    fontSize: 11,
+                    fontWeight: 'bold',
+                    width: 230,
+                  }}>
+                  {item.topic}
                 </Text>
-              </Text>
+                <Text style={{color: AppColor.SECONDARY_COLOR}}>
+                  {item.facilitator}
+                </Text>
+                <Text style={{color: AppColor.SECONDARY_COLOR}}>
+                  {`${item.duration}:00 | ${item.skill} `}
+                  <Text style={{color: AppColor.PRIMARY_COLOR}}>
+                    {item.price}
+                  </Text>
+                </Text>
+              </View>
             </View>
-          </View>
+          </TouchableOpacity>
         )}
       />
     </View>
--- src/bottomnav/VideoPlayerScreen.js
+++ src/bottomnav/VideoPlayerScreen.js
@@ -1,18 +1,84 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, ScrollView, FlatList} from 'react-native';
 import VideoPlayer from 'react-native-video-controls';
+import Icon from 'react-native-vector-icons/dist/MaterialIcons';
+import AppColor from '../modules/AppColor';
 
 const VideoPlayerScreen = ({VideoURI}) => {
   return (
-    <View style={{flex: 0.35}}>
+    <View style={{flex: 1}}>
       <VideoPlayer
-        style={{flex: 1}}
+        style={{height: 300}}
         paused={true}
         controls={true}
-        source={{
-          uri: VideoURI,
-        }}
+        // source={{
+        //   uri: VideoURI.video_file,
+        // }}
       />
+      <View>
+        <Text>{VideoURI.topic}</Text>
+        <Text>{VideoURI.facilitator}</Text>
+        <Text>{'Category Remedial'}</Text>
+        <Text>{`Synopsis \n${VideoURI.description}`}</Text>
+        <View>
+          <Text>{'Comments (5)'}</Text>
+          <View>
+            <TextInput placeholder="Add a comment" />
+          </View>
+        </View>
+        <View style={{backgroundColor: 'powderblue', height: 250}}>
+          <FlatList
+            data={[
+              {
+                id: '1',
+                text:
+                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+              },
+              {
+                id: '2',
+                text:
+                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+              },
+              {
+                id: '3',
+                text:
+                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+              },
+              {
+                id: '4',
+                text:
+                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+              },
+              {
+                id: '5',
+                text:
+                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+              },
+              {
+                id: '6',
+                text:
+                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+              },
+            ]}
+            renderItem={({item}) => (
+              <View style={{flexDirection: 'row'}}>
+                <Icon
+                  name="account-circle"
+                  size={30}
+                  color={AppColor.SECONDARY_COLOR}
+                />
+                <View>
+                  <Text>
+                    {`Anonymous User`}
+                    <Text>2 days ago</Text>
+                  </Text>
+                  <Text>{item.text}</Text>
+                </View>
+              </View>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 };
