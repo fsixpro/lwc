5890da7135515a74be2b10d119573a75f21ebaca
Francis Ayodeji <fsixpro@gmail.com>
2020-09-19 15:34:42 +0100

    video

--- src/bottomnav/VideoPlayerScreen.js
+++ src/bottomnav/VideoPlayerScreen.js
@@ -6,78 +6,103 @@ import AppColor from '../modules/AppColor';
 
 const VideoPlayerScreen = ({VideoURI}) => {
   return (
-    <View style={{flex: 1}}>
-      <VideoPlayer
-        style={{height: 300}}
-        paused={true}
-        controls={true}
-        // source={{
-        //   uri: VideoURI.video_file,
-        // }}
-      />
+    <View>
+      <View style={{height: '35%'}}>
+        <VideoPlayer
+          seekColor={AppColor.PRIMARY_COLOR}
+          disableVolume={true}
+          disableFullscreen={true}
+          paused={true}
+          controls={true}
+          // source={{
+          //   uri: VideoURI.video_file,
+          // }}
+        />
+      </View>
       <View>
         <Text>{VideoURI.topic}</Text>
         <Text>{VideoURI.facilitator}</Text>
         <Text>{'Category Remedial'}</Text>
         <Text>{`Synopsis \n${VideoURI.description}`}</Text>
+        <Text>{'Comments (5)'}</Text>
         <View>
-          <Text>{'Comments (5)'}</Text>
-          <View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              borderWidth: 3,
+              borderColor: AppColor.SECONDARY_COLOR,
+              marginVertical: 4,
+            }}>
+            <Icon
+              style={{marginHorizontal: 15}}
+              name="account-circle"
+              size={25}
+              color={AppColor.SECONDARY_COLOR}
+            />
             <TextInput placeholder="Add a comment" />
           </View>
         </View>
-        <View style={{backgroundColor: 'powderblue', height: 250}}>
-          <FlatList
-            data={[
-              {
-                id: '1',
-                text:
-                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
-              },
-              {
-                id: '2',
-                text:
-                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
-              },
-              {
-                id: '3',
-                text:
-                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
-              },
-              {
-                id: '4',
-                text:
-                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
-              },
-              {
-                id: '5',
-                text:
-                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
-              },
-              {
-                id: '6',
-                text:
-                  "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
-              },
-            ]}
-            renderItem={({item}) => (
-              <View style={{flexDirection: 'row'}}>
-                <Icon
-                  name="account-circle"
-                  size={30}
-                  color={AppColor.SECONDARY_COLOR}
-                />
-                <View>
-                  <Text>
-                    {`Anonymous User`}
-                    <Text>2 days ago</Text>
+      </View>
+      <View style={{height: '33%'}}>
+        <FlatList
+          data={[
+            {
+              id: '1',
+              text:
+                "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+            },
+            {
+              id: '2',
+              text:
+                "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+            },
+            {
+              id: '3',
+              text:
+                "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+            },
+            {
+              id: '4',
+              text:
+                "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+            },
+            {
+              id: '5',
+              text:
+                "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+            },
+            {
+              id: '6',
+              text:
+                "Wow wow wowww!! I have been soooo blessed watching this video. God's Word will NEVER EVER be defeated!",
+            },
+          ]}
+          renderItem={({item}) => (
+            <View
+              style={{
+                flexDirection: 'row',
+                paddingVertical: 10,
+                borderBottomWidth: 1,
+                borderBottomColor: AppColor.SECONDARY_COLOR,
+              }}>
+              <Icon
+                name="account-circle"
+                size={40}
+                color={AppColor.SECONDARY_COLOR}
+              />
+              <View>
+                <Text style={{fontWeight: 'bold', color: AppColor.BLACK}}>
+                  {`Anonymous User  `}
+                  <Text style={{fontSize: 10, color: AppColor.SECONDARY_COLOR}}>
+                    2 days ago
                   </Text>
-                  <Text>{item.text}</Text>
-                </View>
+                </Text>
+                <Text>{item.text}</Text>
               </View>
-            )}
-          />
-        </View>
+            </View>
+          )}
+        />
       </View>
     </View>
   );
